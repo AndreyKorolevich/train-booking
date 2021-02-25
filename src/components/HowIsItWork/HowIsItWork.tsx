2710aafc27f7bf91fe1c46418ee7d897ae8a2ebb
andreykorolevich <korolevich1994@mail.ru>
2021-02-25 23:27:36 +0300

    [about-1]: Added styles for the About and HowIsItWork components

--- src/components/HowIsItWork/HowIsItWork.tsx
+++ src/components/HowIsItWork/HowIsItWork.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
+import cn from 'classnames';
+import styles from './HowIsItWork.module.scss';
 import Button from '../Common/Button/Button';
+import order from '../../img/order.svg';
+import office from '../../img/office.svg';
+import direction from '../../img/direction.svg';
 
 const HowIsItWork: React.FC = () => {
   return (
-    <div className='card'>
-      <div className='card-header'>
-        КАК ЭТО РАБОТАЕТ
-        <Button onClick={() => console.log('kek')} className='btn-primary'>
-          Узнать больше
-        </Button>
-      </div>
-      <div className='card-body'>
-        <ul className='list-group list-group-flush'>
-          <li className='list-group-item'>Удобный заказ на сайте</li>
-          <li className='list-group-item'>Нет необходимости ехать в офис</li>
-          <li className='list-group-item'>Огромный выбор направлений</li>
-        </ul>
+    <div className={styles.work}>
+      <div className='container'>
+        <div className={cn('card', styles.block)}>
+          <div className={cn('card-header', styles.header)}>
+            КАК ЭТО РАБОТАЕТ
+            <Button onClick={() => console.log('kek')} className={cn('btn-outline-light', styles.button)}>
+              Узнать больше
+            </Button>
+          </div>
+          <div className={cn('card-body', styles.body)}>
+            <ul className={cn('list-group list-group-horizontal', styles.list)}>
+              <li className={cn('list-group-item', styles.li)}>
+                <img src={order} alt='print icon' />
+                Удобный заказ на сайте
+              </li>
+              <li className={cn('list-group-item', styles.li)}>
+                <img src={office} alt='print icon' />
+                Нет необходимости ехать в офис
+              </li>
+              <li className={cn('list-group-item', styles.li)}>
+                <img src={direction} alt='print icon' />
+                Огромный выбор направлений
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
